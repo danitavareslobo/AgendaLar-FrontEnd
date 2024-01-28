2955feb3d8ae625a3d936f889b43ee2a3c2640c8
Daniele Tavares Lobo <danitavares.dev@gmail.com>
2024-01-27 22:04:48 -0300

    Implementação ReactiveForms

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   loginCard = true;
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  signupForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+    confirmPassword: ['', Validators.required]
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmitLogin(){
+    console.log("loginForm", this.loginForm.value);
+  }
+
+  onSubmitSignup(){
+    console.log("signupForm", this.signupForm.value);
+  }
 }
